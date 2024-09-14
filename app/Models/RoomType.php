359452e5e7a72d25fae1ba2bf7155f1832eb2422
Anochaakimbo <bbcorn123za@gmail.com>
2024-09-14 19:59:22 +0700

    solve

--- app/Models/RoomType.php
+++ app/Models/RoomType.php
@@ -9,16 +9,13 @@ use Illuminate\Database\Eloquent\Model;
 class RoomType extends Model
 {
     protected $fillable = ['room_description', 'room_price', 'furniture_details', 'deposit_price'];
-    public function rooms()
-    {
-        return $this->hasMany(rooms::class);
+
     use HasFactory;
 
     protected $table = 'room_types'; // ชื่อตาราง
 
     public $timestamps = false;
 
-    protected $fillable = ['room_description', 'room_price'];
 
     // ความสัมพันธ์ hasMany กับ SelectRoom
     public function rooms()
