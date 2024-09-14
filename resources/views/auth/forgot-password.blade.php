b44af6cc02f85aab1e672223054d39a813dd6dc6
Anochaakimbo <bbcorn123za@gmail.com>
2024-09-14 15:47:49 +0700

    changefrontend

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,36 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+้<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        @import url( {{asset('css/forgetpasspage.css')}});
+    </style>
+</head>
+<body>
+    <div class="forgetpassbox">
+    @session('status')
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ $value }}
+        </div>
+    @endsession
+    <form method="POST" action="{{ route('password.email') }}">
+        <img src="./img/Logo.png" alt="">
+        <div class="description">
+            {{ __('หากคุณลืมรหัสผ่าน ให้กรอกอีเมลของคุณที่ใช้สมัคร ทางเราจะส่งลิงค์เปลี่ยนรหัสผ่านไปที่อีเมลของคุณ') }}
+        </div>
+        @csrf
+        <div class="emailbox">
+            <label for="email" value="{{ __('Email') }}">อีเมล:
+            <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="กรอกอีเมลที่ใช้สมัคร"/>
         </div>
 
-        @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ $value }}
-            </div>
-        @endsession
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+            <button class="forgetbutton">
+                {{ __('ส่งอีเมล') }}
+            </button>
+    </form>
+    </div>
+</body>
+</html>
