d21118cc52f9d99c01f30afda5db5924d8168fd8
Anochaakimbo <bbcorn123za@gmail.com>
2024-09-23 17:17:57 +0700

    finished wait for all check and swal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,14 +42,18 @@
             </div>
         </div>
 
-        <!-- Main Content -->
-        <!-- Main Content -->
         <header>
             <h1>ยินดีต้อนรับสู่ระบบ โกลเด้นซิตี้</h1>
         </header>
         <div class="main-content">
             <div class="room-info">
-                <img src="/img/single_room.png" alt="ห้องพัก" class="room-img">
+                @if (Auth::user()->room->roomType->room_description == 'Single')
+                    <img src="./img/singlebedroom.jpg" alt="ห้องพัก Single" class="room-img">
+                @elseif (Auth::user()->room->roomType->room_description == 'Premium')
+                    <img src="./img/premiumbedroom.jpg" alt="ห้องพัก Premium" class="room-img">
+                @elseif (Auth::user()->room->roomType->room_description == 'Twin')
+                    <img src="./img/twinbedroom.jpg" alt="ห้องพัก Twin" class="room-img">
+                @endif
                 <div class="details">
                     <p><strong>ชื่อผู้เข้าพัก:</strong> {{ Auth::user()->name }}</p>
                     <p><strong>ห้อง:</strong> {{ Auth::user()->room->room_number }}</p>
@@ -64,7 +68,6 @@
                     </p>
                     <p><strong>หน่วยค่าไฟ:</strong> {{ Auth::user()->room->roomType->electrical_unit }} บาท/ยูนิต</p>
                     <p><strong>หน่วยค่าน้ำ:</strong> {{ Auth::user()->room->roomType->water_unit }} บาท/ยูนิต</p>
-                    {{-- <p><strong>สถานะแก้ไขปัญหา:</strong> {{ Auth::user()->room->roomType->room_price}}</p> --}}
                 </div>
             </div>
         </div>
